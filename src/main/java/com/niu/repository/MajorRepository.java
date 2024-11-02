d7a0c1f81330b10a3408633b7f3b6e3801559a69
niuniu5 <15601131025@163.com>
2024-11-02 13:47:33 +0800

    add  ignore

--- src/main/java/com/niu/repository/MajorRepository.java
+++ src/main/java/com/niu/repository/MajorRepository.java
@@ -4,11 +4,13 @@ import com.niu.entity.MajorEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MajorRepository extends JpaRepository<MajorEntity, Long> {
-
+    @Query("SELECT c FROM MajorEntity c WHERE (c.collegeId = :collegeId and c.name= :name) " )
+    MajorEntity getMajorByCollegeIdAndName(Integer collegeId, String name);
     MajorEntity getMajorById(Integer id);
     MajorEntity getMajorByName(String name);
     Page<MajorEntity> getMajorByCollegeId(Integer id, Pageable pageable);
